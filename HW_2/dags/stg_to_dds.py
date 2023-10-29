ab5e4504daeefb4499a9281d96cd349a25c43563
NiKoV <nikitachesma70@gmail.com>
2023-10-29 23:50:37 +0500

    add dds showcase

--- HW_2/dags/stg_to_dds.py
+++ HW_2/dags/stg_to_dds.py
@@ -7,26 +7,45 @@ from airflow.operators.python_operator import PythonOperator
 def editors():
     PostgresHook(postgres_conn_id="PG_WAREHOUSE_CONNECTION").run(
         """
+    WITH discipline_data AS (
+    SELECT
+        jsonb_array_elements(wp_list::jsonb) AS discipline
+    FROM
+        stg.su_wp
+    ),
+    editors_data AS (
+    SELECT
+        (jsonb_array_elements(discipline->'editors')->>'id')::integer AS id,
+        jsonb_array_elements(discipline->'editors')->>'username' AS username,
+        jsonb_array_elements(discipline->'editors')->>'first_name' AS first_name,
+        jsonb_array_elements(discipline->'editors')->>'last_name' AS last_name,
+        jsonb_array_elements(discipline->'editors')->>'email' AS email,
+        jsonb_array_elements(discipline->'editors')->>'isu_number' AS isu_number
+    FROM
+        discipline_data
+    )
     INSERT INTO dds.editors (id, username, first_name, last_name, email, isu_number)
-    select distinct (json_array_elements(wp_list::json->'editors')::json->>'id')::integer as editor_id, 
-        (json_array_elements(wp_list::json->'editors')::json->>'username') as username,
-        (json_array_elements(wp_list::json->'editors')::json->>'first_name') as first_name,
-        (json_array_elements(wp_list::json->'editors')::json->>'last_name') as last_name,
-        (json_array_elements(wp_list::json->'editors')::json->>'email') as email,
-        (json_array_elements(wp_list::json->'editors')::json->>'isu_number') as isu_number
-    from stg.su_wp sw
-    ON CONFLICT ON CONSTRAINT editors_uindex DO UPDATE 
+    SELECT DISTINCT ON (id) id, username, first_name, last_name, email, isu_number
+    FROM editors_data
+    WHERE id IS NOT NULL
+    ON CONFLICT (id) DO UPDATE 
     SET 
         username = EXCLUDED.username, 
         first_name = EXCLUDED.first_name,
         last_name = EXCLUDED.last_name, 
         email = EXCLUDED.email, 
         isu_number = EXCLUDED.isu_number;
+
     """
     )
 
 
 def states():
+    PostgresHook(postgres_conn_id="PG_WAREHOUSE_CONNECTION").run(
+        """
+    truncate dds.states restart identity cascade;
+    """
+    )
     PostgresHook(postgres_conn_id="PG_WAREHOUSE_CONNECTION").run(
         """
     INSERT INTO dds.states (cop_state, state_name)
@@ -47,25 +66,6 @@ def states():
     )
 
 
-def units():
-    PostgresHook(postgres_conn_id="PG_WAREHOUSE_CONNECTION").run(
-        """
-    INSERT INTO dds.units (id, unit_title, faculty_id)
-    select 
-        distinct sw.fak_id, 
-        sw.fak_title, 
-        ud.faculty_id::integer
-    from stg.su_wp sw 
-    left join stg.up_description ud 
-    on sw.fak_title = ud.faculty_name 
-    ON CONFLICT ON CONSTRAINT units_uindex DO UPDATE 
-    SET 
-        unit_title = EXCLUDED.unit_title, 
-        faculty_id = EXCLUDED.faculty_id;
-    """
-    )
-
-
 def up():
     PostgresHook(postgres_conn_id="PG_WAREHOUSE_CONNECTION").run(
         """
@@ -74,27 +74,11 @@ def up():
     )
     PostgresHook(postgres_conn_id="PG_WAREHOUSE_CONNECTION").run(
         """
-    INSERT INTO dds.up (id, plan_type, direction_id, ns_id, edu_program_id, edu_program_name, unit_id, level_id, university_partner, up_country, lang, military_department, selection_year)
-    select ud.id, 
-        ud.plan_type, 
-        d.direction_id,
-        ud.ns_id::integer,
-        ud.edu_program_id::integer,
-        ud.edu_program_name,
-        u.id as unit_id,
-        l.id as level_id,
-        ud.university_partner, 
-        ud.up_country, 
-        ud.lang, 
-        ud.military_department, 
-        ud.selection_year::integer
-    from stg.up_description ud 
-    left join dds.directions d 
-    on d.direction_code = ud.direction_code 
-    left join dds.units u 
-    on u.unit_title  = ud.faculty_name 
-    left join dds.levels l 
-    on ud.training_period = l.training_period 
+        INSERT INTO dds.up (app_isu_id, on_check, laboriousness, year, qualification, update_ts)
+        select ap_isu_id, on_check, laboriousness, (json_array_elements(academic_plan_in_field_of_study::json)->>'year')::integer as year,
+        (json_array_elements(academic_plan_in_field_of_study::json)->>'qualification') as qualification,
+        update_ts
+        from stg.up_detail
     """
     )
 
@@ -107,31 +91,41 @@ def wp():
     )
     PostgresHook(postgres_conn_id="PG_WAREHOUSE_CONNECTION").run(
         """
-    INSERT INTO dds.wp (wp_id, discipline_code, wp_title, wp_status, unit_id, wp_description)
+    INSERT INTO dds.wp (wp_id, discipline_code, wp_title, wp_status, unit_id, wp_description, update_ts)
     with wp_desc as (
     select 
         distinct json_array_elements(wp_in_academic_plan::json)->>'id' as wp_id,
         json_array_elements(wp_in_academic_plan::json)->>'discipline_code' as discipline_code,
         json_array_elements(wp_in_academic_plan::json)->>'description' as wp_description,
-        json_array_elements(wp_in_academic_plan::json)->>'status' as wp_status
+        json_array_elements(wp_in_academic_plan::json)->>'status' as wp_status,
+        update_ts 
     from stg.work_programs wp),
+    discipline_data AS (
+    SELECT
+        fak_id,
+        jsonb_array_elements(wp_list::jsonb) AS discipline
+    FROM
+        stg.su_wp
+    ),
     wp_unit as (
     select fak_id,
-        wp_list::json->>'id' as wp_id,
-        wp_list::json->>'title' as wp_title,
-        (wp_list::json->>'discipline_code') as discipline_code
-    from stg.su_wp sw)
-    select wp_desc.wp_id::integer, 
+        discipline::json->>'id' as wp_id,
+        discipline::json->>'title' as wp_title,
+        (discipline::json->>'discipline_code') as discipline_code
+    from discipline_data)
+    select distinct
+    	wp_desc.wp_id::integer,
         wp_desc.discipline_code,
         wp_unit.wp_title,
         s.id as wp_status, 
         wp_unit.fak_id as unit_id,
-        wp_desc.wp_description
+        wp_desc.wp_description,
+        wp_desc.update_ts
     from wp_desc
     left join wp_unit
     on wp_desc.discipline_code = wp_unit.discipline_code
     left join dds.states s 
-    on wp_desc.wp_status = s.cop_state;
+    on wp_desc.wp_status = s.cop_state
     """
     )
 
@@ -149,12 +143,27 @@ def wp_inter():
     )
     PostgresHook(postgres_conn_id="PG_WAREHOUSE_CONNECTION").run(
         """
+    WITH discipline_data AS (
+    SELECT
+        (jsonb_array_elements(wp_list::jsonb)->>'id')::integer AS wp_id,
+        jsonb_array_elements(wp_list::jsonb) AS discipline
+    FROM
+        stg.su_wp
+    ),
+    editor_relations AS (
+    SELECT
+        wp_id,
+        (jsonb_array_elements(discipline->'editors')->>'id')::integer AS editor_id
+    FROM
+        discipline_data
+    )
     INSERT INTO dds.wp_editor (wp_id, editor_id)
-    select (wp_list::json->>'id')::integer as wp_id,
-        (json_array_elements(wp_list::json->'editors')::json->>'id')::integer as editor_id
-    from stg.su_wp
+    SELECT DISTINCT wp_id, editor_id
+    FROM editor_relations
+    WHERE editor_id IS NOT NULL AND wp_id is NOT NULL
     """
     )
+
     PostgresHook(postgres_conn_id="PG_WAREHOUSE_CONNECTION").run(
         """
     INSERT INTO dds.wp_up (wp_id, up_id)
@@ -166,21 +175,21 @@ def wp_inter():
     select t.wp_id, (json_array_elements(wp.academic_plan_in_field_of_study::json)->>'ap_isu_id')::integer as up_id from t
     join stg.work_programs wp
     on t.id = wp.id
+	WHERE wp_id IS NOT NULL AND up_id is NOT NULL
     """
     )
 
 
 with DAG(
     dag_id="stg_to_dds",
-    start_date=pendulum.datetime(2022, 1, 1, tz="UTC"),
+    start_date=pendulum.datetime(2023, 1, 10, tz="UTC"),
     schedule_interval="0 4 * * *",
     catchup=False,
 ) as dag:
-    t3 = PythonOperator(task_id="editors", python_callable=editors)
-    t4 = PythonOperator(task_id="states", python_callable=states)
-    t5 = PythonOperator(task_id="units", python_callable=units)
-    t6 = PythonOperator(task_id="up", python_callable=up)
-    t7 = PythonOperator(task_id="wp", python_callable=wp)
-    t8 = PythonOperator(task_id="wp_inter", python_callable=wp_inter)
+    t1 = PythonOperator(task_id="editors", python_callable=editors)
+    t2 = PythonOperator(task_id="states", python_callable=states)
+    t3 = PythonOperator(task_id="up", python_callable=up)
+    t4 = PythonOperator(task_id="wp", python_callable=wp)
+    t5 = PythonOperator(task_id="wp_inter", python_callable=wp_inter)
 
-[t3, t4, t5] >> t6 >> t7 >> t8
+[t1, t2] >> t3 >> t4 >> t5
--- HW_2/sql/dds_ddl.sql
+++ HW_2/sql/dds_ddl.sql
@@ -4,13 +4,10 @@ ALTER TABLE dds.states ADD CONSTRAINT state_name_uindex UNIQUE (state_name);
 create table dds.editors
 (id integer, username varchar(50), first_name varchar(50), last_name varchar(50), email varchar(50), isu_number varchar(6));
 ALTER TABLE dds.editors ADD CONSTRAINT editors_uindex UNIQUE (id);
-create table dds.units
-(id integer, unit_title varchar(100), faculty_id integer);
-ALTER TABLE dds.units ADD CONSTRAINT units_uindex UNIQUE (id);
 create table dds.up
-(id integer, plan_type varchar(8), direction_id integer, ns_id integer, edu_program_id integer, edu_program_name text, unit_id integer, level_id integer, university_partner text, up_country text, lang text, military_department boolean, selection_year integer);
+(app_isu_id integer, on_check varchar(20), laboriousness integer, year integer, qualification varchar(20), update_ts timestamp);
 create table dds.wp
-(wp_id integer, discipline_code integer, wp_title text, wp_status integer, unit_id integer, wp_description text);
+(wp_id integer, discipline_code integer, wp_title text, wp_status integer, unit_id integer, wp_description text, update_ts timestamp);
 ALTER TABLE dds.wp
 ALTER COLUMN discipline_code TYPE text;
 create table dds.wp_editor
